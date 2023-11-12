381c88ee8906e7103cda0e66afb3436e902fd33c
Unique-Divine <realuniquedivine@gmail.com>
2023-11-12 07:13:43 -0600

    failed ts typing experiment

--- live/shims-vue.d.ts
+++ live/shims-vue.d.ts
@@ -3,3 +3,21 @@ declare module "*.vue" {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+
+// TODO: failed experiment
+//
+// import { ComponentCustomProperties as VueCCP } from "vue"
+// import { Store } from "vuex"
+// import { Page } from "./.vuepress/theme/store/types"
+// declare module "@vue/runtime-core" {
+//
+//   interface State {
+//     current: Page
+//   }
+//   
+//   interface ComponentCustomProperties extends VueCCP {
+//     $store: Store<State>
+//     $withBase: (path: string) => string
+//   }
+// }
