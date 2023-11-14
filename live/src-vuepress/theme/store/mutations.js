ae05f543bef91081594ce64a6d68c62ca4b0878a
Unique-Divine <realuniquedivine@gmail.com>
2023-11-14 06:18:06 -0600

    feat(store/mutations): save sets for allTags and allCategories + TS

--- live/src-vuepress/theme/store/mutations.js
+++ live/src-vuepress/theme/store/mutations.js
@@ -1,13 +1,16 @@
 /* mutations.js
  *
  * This mutations.js file defines Vuex mutations. Mutations are responsible for
- * the state in a deterministic, synchronous manner. Each of the fields
- * exported defines a mutation. For example, the functions mapped by
+ * updating the state in a deterministic, synchronous manner. Each of the fields
+ * exported defines a mutation (fn). For example, the functions mapped by
  * "types.SITE_UPDATE" and "types.PAGE_UPDATE" are mutations.
  */
 import { pick, get } from "lodash";
 import { MUTATION_TYPE } from "./types";
 import { formatPages, formatPage, type, header, filterPosts, footer, social, navigation, } from "./utils"; // @ts-ignore
+/** MUTATIONS: Vuex mutations. Responsible for updating the state in a deterministic,
+ * synchronous manner. Each of object field values defines a mutation (fn)
+ * */
 export const MUTATIONS = {
     [MUTATION_TYPE.SITE_UPDATE]: (state, site) => {
         const themeConfig = get(site, "themeConfig", {});
@@ -25,6 +28,10 @@ export const MUTATIONS = {
         state.params = params;
         const posts = filterPosts(state);
         state.posts = posts.sort((a, b) => postTime(b) - postTime(a));
+        state.posts.forEach((post) => {
+            post.tags.forEach((tag) => state.allTags.add(tag));
+            post.categories.forEach((category) => state.allCategories.add(category));
+        });
         state.nav = navigation(state);
         state.type = type(state);
         state.header = header(state);
